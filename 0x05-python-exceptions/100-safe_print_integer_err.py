d549515ae05145480a5ca1dc60d637788dbf877b
Mussa Milanzi <viewholder74@gmail.com>
2023-10-23 11:32:51 -0700

    Task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+safe_print_integer_err(value):
+    try:
+        print(":d}".format(value))
+        return (True)
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return (False)
