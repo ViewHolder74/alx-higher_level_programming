45086bf7f16a3231fdb27323392c266e14aa7cb6
Mussa Milanzi <viewholder74@gmail.com>
2023-10-24 08:43:13 -0700

    FIX:line space

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,15 @@
 class Square that defines a square
 """
 
+
 class Square:
     """ Square Class"""
 
     def __init__(self, size):
         """
         Private instance attribute: size
-        Instantiation with size (no type/value verification)
         """
         self.__size = size
+        """
+        Instantiation with size (no type/value verification)
+        """
