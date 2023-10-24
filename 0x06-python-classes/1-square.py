a2dff7193a8c8eafcc5f6bc600eac5261e2fced2
Mussa Milanzi <viewholder74@gmail.com>
2023-10-24 08:38:20 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+class Square that defines a square
+"""
 
 class Square:
+    """ Square Class"""
+
     def __init__(self, size):
+        """
+        Private instance attribute: size
+        Instantiation with size (no type/value verification)
+        """
         self.__size = size
