31b99bcd12430fbf69660523f3197ce709e09af0
Mussa Milanzi <viewholder74@gmail.com>
2023-10-30 13:23:19 -0700

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,41 +8,32 @@ class Rectangle:
     @property
     def width(self):
         """returning width value"""
-        
         return (self.__width)
 
     @width.setter
     def width(self, value):
         """assigning with a value."""
-        
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-            
         if value < 0:
             raise ValueError("width must be >= 0")
-            
         self.__width = value
 
     @property
     def height(self):
         """Returning hieght"""
-        
         return (self.__height)
 
     @height.setter
     def height(self, value):
         """setting width to value"""
-        
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-            
         if value < 0:
             raise ValueError("height must be >= 0")
-            
         self.__height = value
 
     def __init__(self, width=0, height=0):
         """instantiating private attribute"""
-        
         self.width = width
         self.height = height
