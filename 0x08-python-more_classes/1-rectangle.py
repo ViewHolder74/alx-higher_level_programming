6361f3c9ad68e21a74d23feacbe6337d412762e7
Mussa Milanzi <103193855+ViewHolder74@users.noreply.github.com>
2023-10-30 17:04:48 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
         """assigning with a value."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -29,7 +29,7 @@ class Rectangle:
         """setting width to value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
