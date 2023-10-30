9a7d98f90a5b664859f30d6ca98b7ad8de574ab4
Mussa Milanzi <103193855+ViewHolder74@users.noreply.github.com>
2023-10-30 22:19:10 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -44,5 +44,5 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """instantiating private attribute"""
         
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
