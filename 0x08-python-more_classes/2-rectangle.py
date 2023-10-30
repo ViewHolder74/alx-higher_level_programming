8cabe1735d7b6a99e4f95d429ac36f04d4e801e8
Mussa Milanzi <viewholder74@gmail.com>
2023-10-30 08:50:19 -0700

    Task 3

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+Task 2: Area and Perimeter.
+"""
+
+
+class Rectangle:
+    """ class that defines a rectangle """
+
+    @property
+    def width(self):
+        """ function that gets width """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ function that sets width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ function that gets height """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ function that sets height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """ Instatiate private attribute """
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ calculate are of rectangle """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """ claculate perimeter of rectangle """
+        a = self.__width * 2
+        b = self.__height * 2
+        return (a + b)
