0104a644fe3fc1076f16d2e8ec88fe2eb68ecb9f
Mussa Milanzi <viewholder74@gmail.com>
2023-10-30 06:52:11 -0700

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Task 1, Real definition of a rectangle."""
+
+
+class Rectangle:
+    """class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """ instantiating private attribute
+        arguments:
+            width = 0
+            height = 0
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """returning width value"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """assigning with a value."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returning hieght"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """setting hieght to value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height muist be >= 0")
+        self.__height = value
