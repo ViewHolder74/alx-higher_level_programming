1789ea220adbfb9fed2149558c2721da9e730d34
Mussa Milanzi <viewholder74@gmail.com>
2023-11-07 12:45:25 -0800

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Task 0: Read File"""
+
+
+def read_file(filename=""):
+    """function that reads a text file
+    (UTF8) and prints it to stdout:
+    """
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        """ opend  afile with readmode """
+
+        f.read()
