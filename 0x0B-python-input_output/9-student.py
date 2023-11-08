a1215a3bdfbd9ec4f062bc308337a32fb52a232f
Mussa Milanzi <viewholder74@gmail.com>
2023-11-08 06:52:58 -0800

    Update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" Task 9: Student to JSON """
+"""Task 9: Student to JSON"""
 
 
 class Student:
-    """ Class that defines a student by"""
+    """A class Student that defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """ Function that instantiate attributes """
+        """A function that initiates public instance attributes"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        that retrieves a dictionary representation of a
-        Student instance (same as 8-class_to_json.py)
-        """
+        """A function that returns dictionary representation of Student"""
 
-        return(self.__dict__)
+        return (self.__dict__)
