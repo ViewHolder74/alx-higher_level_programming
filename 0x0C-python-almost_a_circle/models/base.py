4a9f4b685cf5b8187b321f3c9d32557026379cf0
Mussa Milanzi <viewholder74@gmail.com>
2023-11-11 00:42:47 -0800

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Task 2: Base Class."""
+
+
+class Base:
+    """ First class."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialise and instantiate attributes."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
