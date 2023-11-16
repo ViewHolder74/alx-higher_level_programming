5d89068b733ba49e0fe7145d4ab2121f2ffd63d4
Mussa Milanzi <viewholder74@gmail.com>
2023-11-16 01:56:09 -0800

    Task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Task 2: Base Class."""
 
+import json
+
 
 class Base:
     """ First class."""
@@ -14,3 +16,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Function that returns  the JSON string
+        representation of list_dictionaries.
+        """
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return (json.dumps(list_dictionaries))
