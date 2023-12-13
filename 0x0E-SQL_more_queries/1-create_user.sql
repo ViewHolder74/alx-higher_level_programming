6bfce06c13da50aeaf7ad30b12da16d9d94709b0
Mussa Milanzi <viewholder74@gmail.com>
2023-12-13 08:26:28 -0800

    Update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 -- user_0d_1 have all privileges on MySQL server
 -- The user_0d_1 password is user_0d_1_pwd
 -- if user user_0d_1 already exists, the script not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
