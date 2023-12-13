47974b80df0f1f201b139846f148945692b3bf43
Mussa Milanzi <viewholder74@gmail.com>
2023-12-12 23:40:15 -0800

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- This scrip lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name 
+FROM second_table 
+WHERE name IS NOT NULL AND name != '' 
+ORDER BY score DESC;
