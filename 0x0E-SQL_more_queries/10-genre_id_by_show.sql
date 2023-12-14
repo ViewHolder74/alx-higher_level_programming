884364fcc009c6d925efd40e4b1ea2d29f247db6
Mussa Milanzi <viewholder74@gmail.com>
2023-12-14 03:36:44 -0800

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- THis script lists all shows contatined in hbtn_0d_tvshows that have at least
+-- one genre linked
+-- Each record display tv_shows.title - tv_show_genres.genre_id
+-- Results is sorted by ascending order by
+-- tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genress.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
