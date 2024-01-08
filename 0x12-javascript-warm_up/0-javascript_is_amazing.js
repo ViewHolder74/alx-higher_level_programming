174aac17604a7749a3aa4a8fa99c8cbe09abada0
Mussa Milanzi <viewholder74@gmail.com>
2024-01-08 10:43:04 -0800

    javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // A script that print "Javasript is amazing"
 
-const myVar = "JavaScript is Amazing";
+const myVar = 'JavaScript is Amazing';
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+!#usr/bin/node
+
+/*
+ * script that prints 3 lines
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar)
